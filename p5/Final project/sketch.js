21ea9b726c959e99838451b01084735b3f775b89
gbemi737 <78453294+gbemi737@users.noreply.github.com>
2021-12-07 21:36:40 -0500

    made background

--- p5/Final project/sketch.js
+++ p5/Final project/sketch.js
@@ -1,7 +1,20 @@
+//videos that were helpful to my project
+//https://www.youtube.com/watch?v=ccYLb7cLB1I&list=WL&index=4&t=186s(shiffman)
+
 function setup() {
-  // put setup code here
+createCanvas(600, 600);
 }
 
 function draw() {
-  // put drawing code here
+  background("#B83D61"); 
+    
+loadPixels();//my attempt at making a cool background i need to have a record of all the pixels on the screen
+for(int x = 0; x< width; x= x +1) {
+    for (int y = 0; y < height; y= y +1) {
+   int index = x + y * width;
+        pixels[index] = color()
+    }
+}
+    
+updatePixels();
 }
